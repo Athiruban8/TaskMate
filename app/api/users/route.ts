f5f3613cdef9333f31891a4f5a186a79902bb6ce
Athiruban8 <athiruban.p@gmail.com>
2025-08-31 13:28:55 +0530

    Fix wrong import

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,6 @@
 // app/api/users/route.ts
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export async function POST(req: Request) {
